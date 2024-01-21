c691929e0db074fef68f9bd5ff5370151596a734
CristiManolescu <m_cristian_s96@yahoo.com>
2024-01-21 16:39:07 +0200

    Feat: Open AI setup

--- src/components/GenerateTemplate.tsx
+++ src/components/GenerateTemplate.tsx
@@ -1,17 +1,49 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect } from "react";
 import { Button } from "./Button/Button";
 import { Input } from "./Input/Input";
 import styled from "styled-components";
 import { useRef } from "react";
 import { TemplateContext } from "../App";
+import { ImageGenerateLink, OPENAI_KEY, bannerTemplates } from "../utils/data";
+import openai from "../utils/openai";
 
 export const GenerateTemplate: FC = () => {
   const { template } = useContext(TemplateContext);
   const input = useRef<HTMLInputElement>(null);
   const handleClick = () => {
-    console.log(input.current.value);
-    console.log(template);
+    console.log(findSize.size);
   };
+
+  const findSize = bannerTemplates.find((template) => template.name);
+
+  const fetchImage = async () => {
+    const data = await fetch(ImageGenerateLink, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${OPENAI_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "dall-e-3",
+        prompt: "A cute baby sea otter",
+        n: 1,
+        size: findSize.size,
+      }),
+    });
+    const json = await data.json();
+  };
+
+  const fetchText = async () => {
+    const textQuery = `cerinta chat`;
+
+    const textResult = await openai.chat.completions.create({
+      messages: [{ role: "user", content: textQuery }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(textResult.choices[0].message.content);
+  };
+
   return (
     <GenerateContainer>
       <Input
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,19 +1,18 @@
 export const bannerTemplates = [
   {
     name: "Instagram Post",
-    height: 500,
-    weight: 500,
+    size: "500x500",
   },
   {
     name: "Twitter Header",
-    height: 500,
-    weight: 1500,
+    size: "1500x500",
   },
   {
     name: "Story",
-    height: 1920,
-    weight: 1080,
+    size: "1080x1920",
   },
 ] as const;
 
 export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+
+export const ImageGenerateLink = "https://api.openai.com/v1/images/generations";
